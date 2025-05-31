abc10796a5d9e07671d136cd3fd3b493ef97ddc7
SENG Sourng <120125764+sourngseng@users.noreply.github.com>
2025-05-31 17:37:29 +0700

    Update Category

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    // redirector to the products index page
+    return redirect()->route('products.index');
+})->name('home');
 
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 Route::resource('categories', \App\Http\Controllers\CategoryController::class);
